2b0d7e46fdfc2dd6515f81b006a859b99a72deb6
Rohit Santosh Wakade <vakderohit@gmail.com>
2025-08-18 00:41:15 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.bitezy',
-  appName: 'bitezy-food-ordering',
+  appName: 'bitezy',
   webDir: 'build'
 };
 
